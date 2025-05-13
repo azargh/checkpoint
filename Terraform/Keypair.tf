e673bb0878c6b1f7fd07f8654f7a1b5d8c40304c
Azar <azarghantous@gmail.com>
2025-05-13 20:31:42 +0300

    changed keys a bit

--- Terraform/Keypair.tf
+++ Terraform/Keypair.tf
@@ -1,9 +1,4 @@
-resource "aws_key_pair" "checkpoint-key" {
-  key_name   = "checkpoint-key"
+resource "aws_key_pair" "key" {
+  key_name   = "key"
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOvebyUUsbarD4cCY/XE8w7s6284mhBK08rDpg/NPpIR Azar@DESKTOP-P7N5F72"
-}
-
-resource "aws_key_pair" "github-key" {
-  key_name   = "github-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAzepIW9QDNDVQM/BKqmLrNCVBWtXkocAX7YxmL5HrMm Azar@DESKTOP-P7N5F72"
 }
