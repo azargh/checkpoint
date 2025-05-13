91347c6bdd7d21db760a9848b7187e9c7d9a7576
Azar <azarghantous@gmail.com>
2025-05-13 20:15:31 +0300

    try key see if gitlabv works

--- Terraform/Instance.tf
+++ Terraform/Instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "CheckPoint" {
   ami                    = data.aws_ami.amiID.id
   instance_type          = "t2.micro"
-  key_name               = "checkpoint-key"
+  key_name               = "github-key"
   vpc_security_group_ids = [aws_security_group.checkpoint-sg.id]
   availability_zone      = "us-east-1a"
   tags = {
