7581022c48e3045a5986a518b8ace5838b51ca21
Azar <azarghantous@gmail.com>
2025-05-13 20:18:11 +0300

    change sec grp

--- Terraform/SecGrp.tf
+++ Terraform/SecGrp.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "checkpoint-sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "sshfrommyIP" {
   security_group_id = aws_security_group.checkpoint-sg.id
-  cidr_ipv4         = "79.177.147.33/32"
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
